1c54b214adb062d2a239a96dab1d2da902a0294e
Mücahit Nezir <mucahitnezir@gmail.com>
2020-04-08 18:01:00 +0300

    Created mobile sidebar menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,30 @@
 <template>
   <v-app>
+    <!--Navigation Drawer-->
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      absolute
+      disable-resize-watcher
+      temporary
+    >
+      <v-list dense nav>
+        <v-list-item
+          v-for="(item, index) in menuItems"
+          :key="index"
+          :to="item.to"
+        >
+          <v-list-item-title>{{ item.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!--// Navigation Drawer-->
+
+    <!--App Bar-->
     <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-sm-and-down">
         <v-btn
           v-for="(item, index) in menuItems"
           :key="index"
@@ -14,15 +35,21 @@
           {{ item.text }}
         </v-btn>
       </v-toolbar-items>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer" />
     </v-app-bar>
+    <!--// App Bar-->
+
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
+
+    <!--Footer-->
     <v-footer app fixed>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
+    <!--// Footer-->
   </v-app>
 </template>
 
@@ -30,6 +57,7 @@
 export default {
   data() {
     return {
+      drawer: false,
       title: 'Awesome Nuxt.js',
       menuItems: [
         {
